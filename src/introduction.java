7df5d8ef329ed56f66bcdd280d70f95decfa40da
tristan <86117774+knockout101@users.noreply.github.com>
2024-05-11 13:54:37 -0500

    made directory for source files and added first introduction source file

--- src/introduction.java
+++ src/introduction.java
@@ -1,1 +1,14 @@
 // Introduction
+public class introduction
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        newMethod();
+    }
+
+    public static void newMethod()
+    {
+        System.out.println("Inside the second method");
+    }
+}
