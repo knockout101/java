c3bcdc708f187610e8bd42f809ec25ba2e09d689
Tristan <86117774+knockout101@users.noreply.github.com>
2025-07-01 19:37:19 -0500

    Added more to the in-class GUI project

--- utd_cs2336/in-class-code/ExampleGUI.java
+++ utd_cs2336/in-class-code/ExampleGUI.java
@@ -7,34 +7,76 @@ public class ExampleGUI {
         new GUIComponents();
     }
 }
-    class GUIComponents extends JFrame {
-        // JButton - any J comes from swing
-        // anything without awt
-        JButton okButton, cancelButton;
-        JLabel nameLabel;
-        JTextField nameField;
-        JCheckBox boldCheck, italicCheck;
-        JRadioButton redRadio, yellowRadio;
-        JComboBox<String> classCombo;
+class GUIComponents extends JFrame implements ActionListener, FocusListener {
+    // JButton - any J comes from swing
+    // anything without awt
+    JButton okButton, cancelButton;
+    JLabel nameLabel;
+    JTextField nameField;
+    JCheckBox boldCheck, italicCheck;
+    JRadioButton redRadio, yellowRadio;
+    JComboBox<String> classCombo;
 
-        public GUIComponents() {
-            setTitle("Show GUI");
-            setSize(800, 90);
-            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            setLayout(new FlowLayout());
+    public GUIComponents() {
+        setTitle("Show GUI");
+        setSize(900, 90);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
 
-            okButton = new JButton("OK");
-            cancelButton = new JButton("Cancel");
+        okButton = new JButton("OK");
+        cancelButton = new JButton("Cancel");
 
-            // using default JFrame panel
-            add(okButton);
-            add(cancelButton);
+        // using default JFrame panel
+        add(okButton);
+        add(cancelButton);
 
-            nameLabel = new JLabel("Enter Your Name:");
-            nameField = new JTextField("Type Name Here:", 12);
-            add(nameLabel);
-            add(nameField);
+        nameLabel = new JLabel("Enter Your Name:");
+        nameField = new JTextField("Type Name Here:", 12);
+        add(nameLabel);
+        add(nameField);
 
-            setVisible(true);
+        boldCheck = new JCheckBox("Bold", true); // if checked initially "true"
+        italicCheck = new JCheckBox("Italic", true);
+        add(boldCheck);
+        add(italicCheck);
+        
+        redRadio = new JRadioButton("Red");
+        yellowRadio = new JRadioButton("Yellow");
+        ButtonGroup colorGroup = new ButtonGroup(); // this group makes each check exclusive
+        colorGroup.add(redRadio);
+        colorGroup.add(yellowRadio);
+        add(redRadio);
+        add(yellowRadio);
+
+        String [] classOptions = {"Freshman", "Sophamore", "Junior", "Senior"};
+        classCombo = new JComboBox<>(classOptions);
+        classCombo.setSelectedItem("Freshman");
+        add(classCombo);
+
+        // Add action listener
+        okButton.addActionListener(this);
+        cancelButton.addActionListener(this);
+
+        // Add focus listener
+
+        setVisible(true);
+    }
+    @Override 
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == okButton) {
+            JOptionPane.showMessageDialog(this, "Welcome to GUI components, " + nameField.getText());
         }
+        else if(e.getSource() == cancelButton) {
+            JOptionPane.showMessageDialog(this, "Thanks, bye, " + nameField.getText());
+            nameField.setText("");
+        }
+    }        
+    @Override
+    public void focusGained(FocusEvent e) {
+        nameField.setText("");
+    }
+    @Override
+    public void focusLost(FocusEvent e) {
+        
+    }
 }
